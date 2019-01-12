40c7f5d99102b5798ca3c36e475894507978ffb0
ggodreau <godreau@gmail.com>
2019-01-12 14:41:57 -0600

    50
      - refactored to a class component
      - state still does not update but that's okay

--- src/index.js
+++ src/index.js
@@ -1,51 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Season from './season';
+import React from "react";
+import ReactDOM from "react-dom";
+import Season from "./season";
 
-const App = () => {
-  // check for Geolocation support
-  var lon = 0;
-  var lat = 0;
+class App extends React.Component {
+  render() {
+    var lat = 0;
+    var lon = 0;
 
-  window.onload = function() {
-    if (navigator.geolocation) {
-      console.log('Geolocation is supported!');
-    }
-    else {
-      console.log('Geolocation is not supported for this Browser/OS.');
-    }
-  };
-  
-  window.onmouseover = function() {
-    var startPos = 0;
-    var geoSuccess = function(position) {
-      startPos = position;
-      lon = startPos.coords.longitude;
-      lat = startPos.coords.latitude;
-      console.log("lat is", startPos.coords.latitude, "long is", startPos.coords.longitude);
-      console.log(navigator.geolocation);
-      //document.getElementById('startLat').innerHTML = startPos.coords.latitude;
-      //document.getElementById('startLon').innerHTML = startPos.coords.longitude;
+    window.onload = function() {
+      if (navigator.geolocation) {
+        console.log("Geolocation is supported!");
+      } else {
+        console.log("Geolocation is not supported for this Browser/OS.");
+      }
     };
-    navigator.geolocation.getCurrentPosition(geoSuccess);
-  };
 
-  window.onmousedown = function(event) {
-    console.log('in the aapp');
-    lon = 'XXX';
-    lat = 'YYY';
+    window.onmouseover = function() {
+      var startPos = 0;
+      var geoSuccess = function(position) {
+        startPos = position;
+        lon = startPos.coords.longitude;
+        lat = startPos.coords.latitude;
+        console.log(
+          "lat is",
+          startPos.coords.latitude,
+          "long is",
+          startPos.coords.longitude
+        );
+        console.log(navigator.geolocation);
+        //document.getElementById('startLat').innerHTML = startPos.coords.latitude;
+        //document.getElementById('startLon').innerHTML = startPos.coords.longitude;
+      };
+      navigator.geolocation.getCurrentPosition(geoSuccess);
+    };
+    //the below data binding does not update the state
+    return (
+      <div>
+        what the {lat}
+        {lon} hell
+      </div>
+    );
   }
+}
 
-  return (
-    <div>
-      yolo
-      <Season />
-      { lon }{ lat }
-    </div>
-  )
-};
-
-ReactDOM.render(
-  <App />,
-  document.querySelector('#root')
-);
+ReactDOM.render(<App />, document.querySelector("#root"));
