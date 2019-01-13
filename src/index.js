1865f2bd5adac38d4df069f5670e434be1692241
ggodreau <godreau@gmail.com>
2019-01-13 06:01:48 -0600

    56
      - added error handling to retrieving gps coords
      - did NOT add conditional dom show/hide depending on presence of err
      condition

--- src/index.js
+++ src/index.js
@@ -8,19 +8,27 @@ class App extends React.Component {
     this.state = {
       lat: null,
       lon: null,
-      acc: null
+      acc: null,
+      err: null
     };
     console.log("constructed AF");
 
-    navigator.geolocation.getCurrentPosition(Position => {
-      console.log("Position obj is", Position);
-      console.log("lat is", Position.coords.latitude);
-      this.setState({
-        lat: Position.coords.latitude,
-        lon: Position.coords.longitude,
-        acc: Position.coords.accuracy
-      });
-    });
+    navigator.geolocation.getCurrentPosition(
+      Position => {
+        console.log("Position obj is", Position);
+        console.log("lat is", Position.coords.latitude);
+        this.setState({
+          lat: Position.coords.latitude,
+          lon: Position.coords.longitude,
+          acc: Position.coords.accuracy
+        });
+      },
+      err => {
+        this.setState({
+          err: err.message
+        });
+      }
+    );
 
     window.onload = function() {
       if (navigator.geolocation) {
@@ -38,7 +46,13 @@ class App extends React.Component {
     //the below data binding does not update the state
     return (
       <div>
-        lat is {this.state.lat} lon is {this.state.lon} acc is {this.state.acc}
+        lat is {this.state.lat}
+        <br />
+        lon is {this.state.lon}
+        <br />
+        acc is {this.state.acc}
+        <br />
+        err is {this.state.err}
       </div>
     );
   }
