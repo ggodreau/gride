1aac17cdc88933dd5fd143345b37c37809283fc7
ggodreau <godreau@gmail.com>
2019-01-23 12:13:53 -0600

    76 - added search bar

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import SearchBar from "./SearchBar";
 
 const App = () => {
-  return <div>foo</div>;
+  return (
+    <div>
+      <h1>foo</h1>
+      <SearchBar />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <form>
+        <input placeholder="input me here" />
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
